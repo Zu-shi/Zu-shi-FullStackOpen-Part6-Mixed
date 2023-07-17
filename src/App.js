46fcf6f6d468e1b80fe4258842f99b9166f434a3
Zu-shi <accountistaken@gmail.com>
2023-07-17 17:28:54 -0400

    Complete ex 6.20, retrieve anecdotes through react query and report errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const url = 'http://localhost:3001/anecdotes'
 
 const App = () => {
+
+  const { isLoading, isError, data, error } = useQuery(
+    'anecdotes', () => axios.get(url).then(res => res.data)
+  )
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if (isLoading) {
+    return <div>Anecdotes are being fetched</div>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  const anecdotes = data
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
+
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
