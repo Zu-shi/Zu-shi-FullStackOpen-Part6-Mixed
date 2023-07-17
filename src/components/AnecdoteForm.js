0072570eb9f6b58bd2ca0e63146695c74e3e94d5
Zu-shi <accountistaken@gmail.com>
2023-07-17 17:51:38 -0400

    Complete exercise 6.21, can add anecdotes

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,11 +1,33 @@
+
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { useMutation, useQueryClient } from 'react-query'
+
+const url = 'http://localhost:3001/anecdotes'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  function getId() {
+    return Math.floor(Math.random() * 100000) + 1;
+  }
+
+  const submit = useMutation(
+    (data) => axios.post(url, { content: data, id: getId(), votes: 0 }).then((res) => { return res.data }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('anecdotes')
+      },
+    }
+  )
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    submit.mutate(content)
     event.target.anecdote.value = ''
     console.log('new anecdote')
-}
+  }
 
   return (
     <div>
